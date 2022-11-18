167612985d083eb133bdc580ad3f6f914dd0656f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-18 10:58:21 +0530

    implemented interface and overridden all the methods

--- src/main/java/com/example/demo1/service/StudentServiceImpl.java
+++ src/main/java/com/example/demo1/service/StudentServiceImpl.java
@@ -7,5 +7,25 @@
  */
 package com.example.demo1.service;
 
-public class StudentServiceImpl {
+import com.example.demo1.domain.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements IStudentService{
+	@Override
+	public boolean addStudent(Student student) {
+		return false;
+	}
+
+	@Override
+	public boolean deleteStudent(int id) {
+		return false;
+	}
+
+	@Override
+	public List<Student> fetchAllStudents() {
+		return null;
+	}
 }
