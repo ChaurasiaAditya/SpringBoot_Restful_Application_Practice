6b4c057ad10170cad945c3dda47b5f056c9d9f25
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-18 10:56:01 +0530

    added fetchAllStudent method in Student Repository

--- src/main/java/com/example/demo1/service/IStudentService.java
+++ src/main/java/com/example/demo1/service/IStudentService.java
@@ -2,8 +2,13 @@ package com.example.demo1.service;
 
 import com.example.demo1.domain.Student;
 
+import java.util.List;
+
 public interface IStudentService {
 	boolean addStudent(Student student);
 
 	boolean deleteStudent(int id);
+
+	List<Student> fetchAllStudents();
+
 }
