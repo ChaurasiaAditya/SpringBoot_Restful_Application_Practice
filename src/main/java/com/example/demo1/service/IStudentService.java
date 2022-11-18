93308fdc5ec03d28219c277c2d5fe6697fc5617f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-18 10:54:37 +0530

    added deleteStudent method in Student Repository

--- src/main/java/com/example/demo1/service/IStudentService.java
+++ src/main/java/com/example/demo1/service/IStudentService.java
@@ -3,5 +3,7 @@ package com.example.demo1.service;
 import com.example.demo1.domain.Student;
 
 public interface IStudentService {
-	void addStudent(Student student);
+	boolean addStudent(Student student);
+
+	boolean deleteStudent(int id);
 }
